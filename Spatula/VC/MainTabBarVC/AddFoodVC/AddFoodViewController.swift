1506d2a8d4f0add5af1b7940d07c3585d9cf3106
dasol02 <leedasol2169@gmail.com>
2021-03-18 08:00:49 +0900

    # Add AddFood View

--- Spatula/VC/MainTabBarVC/AddFoodVC/AddFoodViewController.swift
+++ Spatula/VC/MainTabBarVC/AddFoodVC/AddFoodViewController.swift
@@ -4,12 +4,14 @@ class AddFoodViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Stack Add Food Height 75 (65 + (margin 5 * 2))
 
 //        let bodyInfoViewController = UIStoryboard.init(name: "BodyInfo", bundle: nil).instantiateViewController(withIdentifier: "BodyInfoViewController") as! BodyInfoViewController
 //        self.navigationController?.pushViewController(bodyInfoViewController, animated: true)
         
-        let searchViewController = UIStoryboard.init(name: "Search", bundle: nil).instantiateViewController(withIdentifier: "SearchViewController") as! SearchViewController
-        self.navigationController?.pushViewController(searchViewController, animated: true)
+//        let searchViewController = UIStoryboard.init(name: "Search", bundle: nil).instantiateViewController(withIdentifier: "SearchViewController") as! SearchViewController
+//        self.navigationController?.pushViewController(searchViewController, animated: true)
         
         // Do any additional setup after loading the view.
     }
